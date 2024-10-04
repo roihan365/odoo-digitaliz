78886ba9156c15b3707bc765129ace92acd27c2d
roihan365 <bitcoiner.banjarmasin@gmail.com>
2024-10-04 21:36:06 +0800

    refactor: add cors headers

--- controllers/employee_controller.py
+++ controllers/employee_controller.py
@@ -2,22 +2,24 @@ from odoo import http
 from odoo.http import request
 import json
 
-class EmployeeController(http.Controller):
-
-    import json
-from odoo import http
-from odoo.http import request
-
 class ApiEmployeeController(http.Controller):
 
-    @http.route('/api/employee/<int:employee_id>', type='http', auth='apikey', methods=['GET'], csrf=False, cors='*')
+    # Utility function to add CORS headers
+    def _set_cors_headers(self, response):
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+        response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization, X-Requested-With'
+        response.headers['Access-Control-Allow-Credentials'] = 'true'
+        return response
+
+    @http.route('/api/employee/<int:employee_id>', type='http', auth='apikey', methods=['GET'], csrf=False)
     def get_employee(self, employee_id, **kwargs):
         try:
             # Fetching the employee record based on ID
             employee = request.env['hr.employee'].sudo().browse(employee_id)
 
             if employee.exists():
-                return request.make_response(
+                response = request.make_response(
                     json.dumps({
                         'status': 'success',
                         'data': {
@@ -32,16 +34,19 @@ class ApiEmployeeController(http.Controller):
                     }),
                     headers=[('Content-Type', 'application/json')]
                 )
+                return self._set_cors_headers(response)
             else:
-                return request.make_response(
+                response = request.make_response(
                     json.dumps({
                         'status': 'error',
                         'message': 'Record not found'
                     }),
                     headers=[('Content-Type', 'application/json')]
                 )
+                return self._set_cors_headers(response)
+
         except Exception as e:
-            return request.make_response(
+            response = request.make_response(
                 json.dumps({
                     'status': 'error',
                     'message': str(e)  # This will help you capture the actual error
@@ -49,9 +54,9 @@ class ApiEmployeeController(http.Controller):
                 headers=[('Content-Type', 'application/json')],
                 status=500  # Return a 500 status code
             )
+            return self._set_cors_headers(response)
 
-
-    @http.route('/api/employee/all', type='http', auth='apikey', methods=['GET'], csrf=False, cors='*')
+    @http.route('/api/employee/all', type='http', auth='apikey', methods=['GET'], csrf=False)
     def get_all_employees(self, **kwargs):
         try:
             employees = request.env['hr.employee'].sudo().search([])
@@ -66,19 +71,21 @@ class ApiEmployeeController(http.Controller):
                 'position': employee.job_id.name if employee.job_id else None,
             } for employee in employees]
 
-            return request.make_response(
+            response = request.make_response(
                 json.dumps({
                     'status': 'success',
                     'data': employee_list
                 }),
                 headers=[('Content-Type', 'application/json')]
             )
+            return self._set_cors_headers(response)
 
         except Exception as e:
-            return request.make_response(
+            response = request.make_response(
                 json.dumps({
                     'status': 'error',
                     'message': str(e)
                 }),
                 headers=[('Content-Type', 'application/json')]
             )
+            return self._set_cors_headers(response)
